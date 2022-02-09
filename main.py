ba3f12c2e7f1679fe8e2f70e0f46ce74eaf72a7f
jon-pruessmeier <pruessmeierjon@gmail.com>
2022-02-09 01:57:52 +0100

    externalized the testdata into an own file

--- main.py
+++ main.py
@@ -1,31 +1,6 @@
 import json
 import sys
-
-data = [
-    {
-        'title': 'Staatstheorie',
-        'picture': '',
-        'text': "Eine Staatstheorie oder Staatsphilosophie behandelt mögliche Definitionen, Entstehung, Formen, Aufgaben und Ziele des Staates sowie dessen institutionelle, soziale, ethische und juristische Bedingungen und Grenzen. Als Teilgebiet der Politischen Philosophie und Konkretion der Allgemeinen Staatslehre berühren Staatstheorien deshalb oftmals Fragestellungen, die mehrere Einzelwissenschaften gleichzeitig betreffen, darunter: die Philosophie, die Theologie, die Politikwissenschaft, die Rechtswissenschaft, die Soziologie und die Volkswirtschaftslehre."
-    },
-    {
-        'title': 'Staat',
-        'picture': 'https://de.wikipedia.org/wiki/Staat#/media/Datei:Leviathan_by_Thomas_Hobbes.jpg',
-        'text': 'Staat (ugs. bzw. nicht fachspr. auch Land) ist ein mehrdeutiger Begriff verschiedener Sozial- und Staatswissenschaften. Im weitesten Sinn bezeichnet er eine politische Ordnung, in der einer bestimmten Gruppe, Organisation oder Institution eine privilegierte Stellung zukommt – nach Ansicht einiger bei der Ausübung von (politischer) Macht; nach Ansicht anderer hinsichtlich sowohl der Entfaltung des Einzelnen als auch der Gesellschaft.'
-    },
-
-    {
-        'title': 'Thomas Hobbes',
-        'picture': 'https://upload.wikimedia.org/wikipedia/commons/d/d6/Thomas_Hobbes_by_John_Michael_Wright_%282%29.jpg',
-        'text': 'Thomas Hobbes [hɔbz] (5. April 1588 in Westport, Wiltshire – 4. Dezember 1679 in Hardwick Hall, Derbyshire) war ein englischer Mathematiker, Staatstheoretiker und Philosoph. Er wurde durch sein Hauptwerk Leviathan bekannt, in dem er eine Theorie des Absolutismus entwickelte. Er gilt als Begründer des „aufgeklärten Absolutismus“.[1] Des Weiteren ist er neben John Locke und Jean-Jacques Rousseau einer der bedeutendsten Theoretiker des sogenannten Gesellschaftsvertrags.'
-    },
-
-    {
-        'title': '',
-        'picture': '',
-        'text': '',
-
-    }
-]
+from testdata import data
 data_json = json.dumps(data)
 print(data_json)
 
---
+++ testdata.py
@@ -0,0 +1,83 @@
+data = [
+    {
+        'title': 'Staatstheorie',
+        'picture': '',
+        'text': "Eine Staatstheorie oder Staatsphilosophie behandelt mögliche Definitionen, Entstehung, Formen, Aufgaben und Ziele des Staates sowie dessen institutionelle, soziale, ethische und juristische Bedingungen und Grenzen. Als Teilgebiet der Politischen Philosophie und Konkretion der Allgemeinen Staatslehre berühren Staatstheorien deshalb oftmals Fragestellungen, die mehrere Einzelwissenschaften gleichzeitig betreffen, darunter: die Philosophie, die Theologie, die Politikwissenschaft, die Rechtswissenschaft, die Soziologie und die Volkswirtschaftslehre."
+    },
+    {
+        'title': 'Staat',
+        'picture': 'https://de.wikipedia.org/wiki/Staat#/media/Datei:Leviathan_by_Thomas_Hobbes.jpg',
+        'text': 'Staat (ugs. bzw. nicht fachspr. auch Land) ist ein mehrdeutiger Begriff verschiedener Sozial- und Staatswissenschaften. Im weitesten Sinn bezeichnet er eine politische Ordnung, in der einer bestimmten Gruppe, Organisation oder Institution eine privilegierte Stellung zukommt – nach Ansicht einiger bei der Ausübung von (politischer) Macht; nach Ansicht anderer hinsichtlich sowohl der Entfaltung des Einzelnen als auch der Gesellschaft.'
+    },
+
+    {
+        'title': 'Thomas Hobbes',
+        'picture': 'https://upload.wikimedia.org/wikipedia/commons/d/d6/Thomas_Hobbes_by_John_Michael_Wright_%282%29.jpg',
+        'text': 'Thomas Hobbes [hɔbz] (5. April 1588 in Westport, Wiltshire – 4. Dezember 1679 in Hardwick Hall, Derbyshire) war ein englischer Mathematiker, Staatstheoretiker und Philosoph. Er wurde durch sein Hauptwerk Leviathan bekannt, in dem er eine Theorie des Absolutismus entwickelte. Er gilt als Begründer des „aufgeklärten Absolutismus“.[1] Des Weiteren ist er neben John Locke und Jean-Jacques Rousseau einer der bedeutendsten Theoretiker des sogenannten Gesellschaftsvertrags.'
+    },
+
+    {
+        'title': 'Aminosäuren',
+        'picture': 'https://upload.wikimedia.org/wikipedia/commons/c/ce/AminoAcidball.svg',
+        'text': """
+        Aminosäuren (AS), unüblich aber genauer auch Aminocarbonsäuren, veraltet Amidosäuren genannt, sind chemische Verbindungen mit einer Stickstoff (N) enthaltenden Aminogruppe und einer Kohlenstoff (C) und Sauerstoff (O) enthaltenden Carbonsäuregruppe.[1] Aminosäuren kommen in allen Lebewesen vor. Sie sind die Bausteine von Proteinen (Eiweiß) und werden frei bei der Zerlegung von Proteinen (Proteolyse). Essentielle Aminosäuren kann ein Organismus nicht selber herstellen, sie müssen daher mit der Nahrung aufgenommen werden.
+
+Zur Klasse der Aminosäuren zählen organische Verbindungen, die zumindest eine Aminogruppe (–NH2 bzw. substituiert –NR2) und eine Carboxygruppe (–COOH) als funktionelle Gruppen enthalten, also Strukturmerkmale der Amine und der Carbonsäuren aufweisen. Chemisch lassen sie sich nach der Stellung ihrer Aminogruppe zur Carboxylgruppe unterscheiden – steht die Aminogruppe am Cα-Atom unmittelbar benachbart zur endständigen Carboxygruppe, nennt man dies α-ständig und spricht von α-Aminosäuren.
+
+Ausgewählte α-Aminosäuren sind die natürlichen Bausteine von Proteinen. Sie werden miteinander zu Ketten verknüpft, indem die Carboxygruppe der einen Aminosäure mit der Aminogruppe der nächsten eine Peptidbindung eingeht. Die auf diese Weise zu einem Polymer verketteten Aminosäuren unterscheiden sich in ihren Seitenketten und bestimmen zusammen die Form, mit der das Polypeptid im wässrigen Milieu dann zum nativen Protein auffaltet. Diese Biosynthese von Proteinen findet in allen Zellen an den Ribosomen nach Vorgabe genetischer Information statt, die in Form von mRNA vorliegt.
+
+Die Basensequenz der mRNA codiert in Tripletts die Aminosäurensequenz, wobei jeweils ein Basentriplett ein Codon darstellt, das für eine bestimmte proteinogene Aminosäure steht. Die hiermit als Bausteine für die Bildung von Proteinen in einer bestimmten Reihenfolge angegebenen Aminosäuren formen die Proteine.[2] Beim Menschen sind es 21 verschiedene proteinogene Aminosäuren, neben den standardmäßig 20 (kanonischen) Aminosäuren auch Selenocystein. Nach der Translation können die Seitenketten einiger im Protein eingebauter Aminosäuren noch modifiziert werden.
+
+Das Spektrum der Aminosäuren geht allerdings über diese rund zwanzig proteinogenen weit hinaus. So sind bisher über 400 nichtproteinogene natürlich vorkommende Aminosäuren bekannt, die biologische Funktionen haben.[3] Die vergleichsweise seltenen D-Aminosäuren stellen hierbei eine spezielle Gruppe dar.[4] Die Vielfalt der synthetisch erzeugten und die der theoretisch möglichen Aminosäuren ist noch erheblich größer.
+
+Einige Aminosäuren spielen als Neurotransmitter eine besondere Rolle, ebenso verschiedene Abbauprodukte von Aminosäuren; biogene Amine treten nicht nur als Botenstoffe im Nervensystem auf, sondern entfalten auch als Hormone und Gewebsmediatoren vielfältige physiologische Wirkungen im Organismus.
+
+Die einfachste Aminosäure, Glycin, konnte nicht nur auf der Erde, sondern auch auf Kometen, Meteoriten und in Gaswolken im interstellaren Raum nachgewiesen werden.[5]
+        """,
+
+    },
+
+    {
+        'title': 'Svante Arrhenius',
+        'picture': 'https://upload.wikimedia.org/wikipedia/commons/9/92/Svante_Arrhenius_01.jpg',
+        'text': """
+        Svante August Arrhenius (Aussprache: [ˌsvanːtə aˈɹeːniɵs]; * 19. Februar 1859 auf Gut Wik bei Uppsala; † 2. Oktober 1927 in Stockholm) war ein schwedischer Physiker und Chemiker. 1903 erhielt er den Nobelpreis für Chemie. Er wies nach, dass in Wasser gelöste Salze als Ionen vorliegen. Die Salze zerfallen im Wasser vielfach nicht vollständig in Ionen, sondern nur – abhängig von der Konzentration – zu einem bestimmten Prozentsatz; Arrhenius prägte hierfür das Wort Aktivitätskoeffizient. 1896 sagte er als Erster eine globale Erwärmung aufgrund der anthropogenen Kohlendioxid-Emission voraus.[1][2]""",
+
+    },
+
+    {
+        'title': '',
+        'picture': '',
+        'text': '',
+
+    },
+
+    {
+        'title': 'Hydrolyse',
+        'picture': '',
+        'text': 'Die Hydrolyse (altgriechisch ὕδωρ hydor „Wasser“ und λύσις lýsis „Lösung, Auflösung, Beendigung“) ist die Spaltung einer (bio)chemischen Verbindung durch Reaktion mit Wasser.[1] Dabei wird (formal) ein Wasserstoffatom an das eine „Spaltstück“ abgegeben, der verbleibende Hydroxyrest an das andere Spaltstück gebunden. Die Umkehrung der Hydrolyse ist eine Kondensationsreaktion. Sofern bei der Reaktion auch das Lösungsmittel Wasser ist, zählt die Hydrolyse zu den Solvolysen.[2]',
+
+    },
+
+    {
+        'title': '',
+        'picture': '',
+        'text': '',
+
+    },
+
+    {
+        'title': '',
+        'picture': '',
+        'text': '',
+
+    },
+
+    {
+        'title': '',
+        'picture': '',
+        'text': '',
+
+    },
+
+]
