5be74b07eb6b4b80798d683c77f70bf3208837da
Jón Prüßmeier <jon.pruessmeier@aconso.com>
2022-02-09 12:15:55 +0100

    added comments & made the geckodriver-path from absolute to an variable that has to be found via the os-module

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import json
+import os
 import sys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-PATH = 'C:\\Users\\jopr299\\Desktop\\Selenium-Übungen\\geckodriver.exe'
+# Get the geckodriver from the PATH-Variables
+PATH = os.environ.get('geckodriver.exe')
 
+# Create the driver
 driver = webdriver.Firefox()
+
+# Open the Wikipedia-Mainpage
 driver.get('https://de.wikipedia.org/wiki/Wikipedia:Hauptseite')
 print(driver.title)
+
+# Find the div of the article of the day
 article = driver.find_element(By.ID,'artikel')
 print("Hauptartikel:")
 print(article)
+
+
 print("---------------")
+
+# Find the div which has the anchor element of the link in it
 hauptseite_mehr = driver.find_element(By.CLASS_NAME, 'hauptseite-mehr')
-print("LINK:")
 print(hauptseite_mehr.text)
 driver.quit()
 
