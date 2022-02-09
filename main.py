8df501f01f8e2c0a919d54915338f9dae91d707b
Jón Prüßmeier <jon.pruessmeier@aconso.com>
2022-02-09 11:26:36 +0100

    done work to the project structure (dependencies, venv, etc.)

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 import json
 import sys
-from testdata import data
-data_json = json.dumps(data)
-print(data_json)
+from selenium import webdriver
 
-sys.stdout.flush()
+PATH = 'C:\\Users\\jopr299\\Desktop\\Selenium-Übungen\\geckodriver.exe'
+
+driver = webdriver.Firefox()
+driver.get('https://de.wikipedia.org/wiki/Wikipedia:Hauptseite')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from testdata import data
+# data_json = json.dumps(data)
+# print(data_json)
+
+# sys.stdout.flush()
