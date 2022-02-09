ef4859ac4c26633e8b4e79528b1174e352437b12
Jón Prüßmeier <jon.pruessmeier@aconso.com>
2022-02-09 12:08:07 +0100

    first crawls of wikipedia

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import json
 import sys
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 PATH = 'C:\\Users\\jopr299\\Desktop\\Selenium-Übungen\\geckodriver.exe'
 
 driver = webdriver.Firefox()
 driver.get('https://de.wikipedia.org/wiki/Wikipedia:Hauptseite')
+print(driver.title)
+article = driver.find_element(By.ID,'artikel')
+print("Hauptartikel:")
+print(article)
+print("---------------")
+hauptseite_mehr = driver.find_element(By.CLASS_NAME, 'hauptseite-mehr')
+print("LINK:")
+print(hauptseite_mehr.text)
+driver.quit()
 
 
 
