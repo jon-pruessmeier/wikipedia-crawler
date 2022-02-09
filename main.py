ffb5be742aa0f0fe59e053ef5fd0226698929711
Jón Prüßmeier <jon.pruessmeier@aconso.com>
2022-02-09 14:36:29 +0100

    iteration now stops if an exception occurs and returns all content that is crawled until that

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import sys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+# from testdata import data
+
+#Quantity of pages that shall be crawled
+page_quantity = 10
+
 # Get the geckodriver from the PATH-Variables
 PATH = os.environ.get('geckodriver.exe')
 
@@ -33,58 +38,56 @@ hauptseite_mehr = driver.find_element(By.CLASS_NAME, 'hauptseite-mehr')
 link = driver.find_element(By.LINK_TEXT, hauptseite_mehr.text)
 
 
-def fill_result(link, n):
-    current_link = link
+def fill_result(link_to_page, n):
+    current_link = link_to_page
     result = []
+    try:
+        #Iterating through the pages:
+        for i in range(n):
 
-    #Iterating through the pages:
-    for i in range(n):
+            #Printing the current page-link and saving it in a variable:
+            href = current_link.get_attribute("href")
+            print('-----PAGE LINK:--------')
+            print(href)
 
-        # Accessing the page:
-        current_link.click()
+            # Accessing the page:
+            current_link.click()
 
-        #Printing the actual page-link:
-        print('-----PAGE LINK:--------')
-        print(current_link.get_attribute('href'))
+            # Accessing and printing the title of the article
+            title = driver.find_element(By.ID, title_id)
+            title_text = title.text
+            #print('----------------')
+            #print('TITLE:')
+            #print(title_text)
 
-        # Accessing and printing the title of the article
-        title = driver.find_element(By.ID, title_id)
-        title_text = title.text
-        #print('----------------')
-        #print('TITLE:')
-        #print(title_text)
+            # Accessing and printing the p-element of the article
+            first_p_elem = driver.find_element(By.XPATH, first_p_path)
+            p_text = first_p_elem.text
+            #print('----------------')
+            #print('TEXT:')
+            #print(p_text)
 
-        # Accessing and printing the p-element of the article
-        first_p_elem = driver.find_element(By.XPATH, first_p_path)
-        p_text = first_p_elem.text
-        #print('----------------')
-        #print('TEXT:')
-        #print(p_text)
+            # Accesing a random link to a new wikipedia page and assign it to the current_link variable
+            page_link = driver.find_element(By.XPATH, first_a_path)
+            current_link = page_link
 
-        # Accesing a random link to a new wikipedia page and assign it to the current_link variable
-        current_link = driver.find_element(By.XPATH, first_a_path)
 
-        result.append({'title':title_text, 'p-text':p_text})
-    
+            # Filling the result:
+            result.append({'title':title_text, 'p-text':p_text, 'link': href})
+    except:
+        return result
+
     return result
 
         
-result = fill_result(link, 10)
-#print(result)
+result = fill_result(link, page_quantity)
+print(result)
 
 
 # Close the driver
 driver.quit()
 
 
-
-
-
-
-
-
-
-# from testdata import data
 # data_json = json.dumps(data)
 # print(data_json)
 
