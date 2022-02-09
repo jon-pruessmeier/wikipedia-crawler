a054f9d85f6f17ca540ec2fc31b702dbb9deb088
Jón Prüßmeier <jon.pruessmeier@aconso.com>
2022-02-09 12:45:15 +0100

    accessing the page of the article of the day and printing it's first p-element

--- main.py
+++ main.py
@@ -25,13 +25,29 @@ print("---------------")
 # Find the div which has the anchor element of the link in it
 hauptseite_mehr = driver.find_element(By.CLASS_NAME, 'hauptseite-mehr')
 print(hauptseite_mehr.text)
+link = driver.find_element(By.LINK_TEXT, hauptseite_mehr.text)
+print(link)
+
+
+# Accessing the page of the articel of the day:
+link.click()
+
+# Accessing and printing the p-element of the article
+first_P = driver.find_element(By.XPATH, '/html/body/div[3]/div[3]/div[5]/div[1]/p[1]')
+print('----------------')
+print(first_P.text)
+
+
+def fill_result(link, n):
+    current_link = link
+    result = []
+
+    return None
+
+
+
+# Close the driver
 driver.quit()
-
-
-
-
-
-
 
 
 
